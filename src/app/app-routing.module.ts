8408ccba3c8adad168ccacdc671b3163298b6cf4
JheysonJhair <jhair2003unamba@gmail.com>
2023-07-03 18:50:16 -0500

    Creando el componente aside con style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideoPlayerComponent } from './components/video-player/video-player.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , component: VideoPlayerComponent},
+  { path: '**', redirectTo: '/', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AsideComponent } from './components/aside/aside.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { VideoPlayerComponent } from './components/video-player/video-player.component';
+import { MusicPlayerComponent } from './components/music-player/music-player.component';
+import { HeadComponent } from './components/head/head.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsideComponent,
+    VideoPlayerComponent,
+    MusicPlayerComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
